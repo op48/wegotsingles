ca451617160b7eb9ac93f20251ff360ce905d214
op48 <lianne.tan@gmail.com>
2014-06-02 15:39:28 +0100

    Created steps for profile feature

--- features/profile.feature
+++ features/profile.feature
@@ -1,5 +1,5 @@
 Feature: Profile page
- 
+ @wip
  Scenario: A visitor sees a profile 
    Given We are in the profile page
      Then We should see the user's first name
---
+++ features/step_definitions/profile_steps.rb
@@ -0,0 +1,20 @@
+Given(/^We are in the profile page$/) do
+  #pending # express the regexp above with the code you wish you had
+  
+end
+
+Then(/^We should see the user's first name$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^We should see the user's last name$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^We should see the user's age$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^We should see the user's about$/) do
+  pending # express the regexp above with the code you wish you had
+end

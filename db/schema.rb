6a2be386203801e6822546ebca492aa737a18aab
op48 <lianne.tan@gmail.com>
2014-06-02 17:16:42 +0100

    migrated a image_url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602150908) do
+ActiveRecord::Schema.define(version: 20140602160442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140602150908) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "image_url"
   end
 
 end

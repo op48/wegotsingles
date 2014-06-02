a56e2b862282b154d71c5515f22a37ee36232a60
op48 <lianne.tan@gmail.com>
2014-06-02 15:34:47 +0100

    Created a database file

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140602142456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "gender"
+    t.text     "about"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

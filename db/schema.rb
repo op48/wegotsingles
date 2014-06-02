72398ef6fcb431e4576371f2849e44e22b235177
op48 <lianne.tan@gmail.com>
2014-06-02 16:35:51 +0100

    migrated username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602142456) do
+ActiveRecord::Schema.define(version: 20140602150908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140602142456) do
     t.text     "about"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
 end

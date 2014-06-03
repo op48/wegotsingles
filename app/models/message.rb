d68c61f00440315ead6e0f7e897294023c6087c4
Zoltan Biber <zoltan.biber@gmail.com>
2014-06-03 18:15:11 +0100

    added associations with options

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
+  has_many :replies, :class_name => "Message"
+  belongs_to :original_message, :class_name => "Message", :foreign_key => :message_id
 end

f5f5bd2eaf24efbd54ef238bba3d36eafb54f2a6
Zoltan Biber <zoltan.biber@gmail.com>
2014-06-03 18:16:00 +0100

    added to the rspec test /// test is passing

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Message do
-  it {should belong_to(:user)}
+  it { should belong_to(:user) }
+  it { should have_many(:replies) }
+  it { should belong_to(:original_message) }
 end

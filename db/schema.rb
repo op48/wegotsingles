c1304f771d762b51c117917e838ef5a54d4e41a5
Zoltan Biber <zoltan.biber@gmail.com>
2014-06-04 17:45:11 +0100

    added recipient_id column to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604093425) do
+ActiveRecord::Schema.define(version: 20140604164334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140604093425) do
     t.datetime "updated_at"
     t.integer  "message_id"
     t.boolean  "read",            default: false
+    t.integer  "recipient_id"
   end
 
   create_table "users", force: true do |t|

027b67bd214d18ba6dad1041144c1de436f777f9
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-04 09:10:46 +0100

    add validation for presence of username to registration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  it {should have_many(:messages)}
-  it {should validate_uniqueness_of(:username)}
+  it { should have_many(:messages) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:username) }
 
 end

cd5ec71b161967c5a71335956efe06a4b8bfd44c
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-04 15:50:49 +0100

    TEST: validate presence of preference at registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	has_many :messages
   validates_uniqueness_of :username
   validates_presence_of :username
+  validates_presence_of :preference
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- features/step_definitions/sign_up_no_preference_steps.rb
+++
@@ -1,4 +0,0 @@
-When(/^a "(.*?)" completes the form without stipulating a gender preference$/) do |user|
-  pending # express the regexp above with the code you wish you had
-end
-

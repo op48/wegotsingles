016b1bf545e5c85d7328d884e51c03376221d1c3
op48 <lianne.tan@gmail.com>
2014-06-05 23:08:12 +0100

    Added 'imperial_height' to 'allowed params'.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,11 @@ class UsersController < ApplicationController
     @user = current_user
     @user.update(allowed_params)
     redirect_to me_user_path
-     
   end
 
   private
   def allowed_params #whitelist
-    params.require(:user).permit(:first_name, :last_name, :username, :age, :gender, :preference, :image_url, :about, :height) #for each attribute in the model /white_listed
+    params.require(:user).permit(:first_name, :last_name, :username, :age, :type, :preference, :image_url, :about, :height, :imperial_height) #for each attribute in the model /white_listed
   end 
 
 end

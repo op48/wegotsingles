1cab5ad88fbfb33bdd3b96bd79f2c4837e1266fd
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-05 15:49:57 +0100

    schema & structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604123643) do
+ActiveRecord::Schema.define(version: 20140605144615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20140604123643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "languages", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "messages", force: true do |t|
@@ -38,6 +43,11 @@ ActiveRecord::Schema.define(version: 20140604123643) do
     t.datetime "updated_at"
     t.integer  "ethnicity_id"
     t.integer  "user_id"
+  end
+
+  create_table "user_languages", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|

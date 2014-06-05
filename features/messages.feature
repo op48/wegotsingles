99cd06568ff331d7031b1085d8bd4c8c2505a466
Damilola Odelola <damiiodelola@gmail.com>
2014-06-05 10:31:06 +0100

    resolving merge

--- features/messages.feature
+++ features/messages.feature
@@ -25,4 +25,5 @@ Feature: Messages
   Scenario: A user sees their outbox
     Given that we are on the inbox page
       And we select "outbox"
+      And there are messages in his outbox 
     Then we should view the sent messages
--- features/step_definitions/outbox_steps.rb
+++ features/step_definitions/outbox_steps.rb
@@ -2,6 +2,12 @@ Given(/^that we are on the inbox page$/) do
   visit '/messages'
 end
 
+Given(/^there are messages in his outbox$/) do
+  @message = Message.new(:subject => "Hi", :body => "Hi Bonnie", :reciever_username => "Bonnie")
+  @user.messages << @message
+end
+
 Then(/^we should view the sent messages$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page.has_content?(@message.subject)).to be true
+  expect(page.has_content?(@message.reciever_username)).to be true
 end

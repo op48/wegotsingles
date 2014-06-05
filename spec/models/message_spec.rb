61712bb123abfa0d91287d3569e2cfcbeabdf2fb
Zoltan Biber <zoltan.biber@gmail.com>
2014-06-05 10:21:12 +0100

    added associations to messages / rspec test passing

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Message do
-  it { should belong_to(:user) }
   it { should have_many(:replies) }
   it { should belong_to(:original_message) }
+  it { should belong_to(:sender).class_name('User').with_foreign_key('sender_id') } 
+  it { should belong_to(:receiver).class_name('User').with_foreign_key('recipient_id') } 
 end

96d548553312a9e32dbab68fab035fb1af09b29b
op48 <lianne.tan@gmail.com>
2014-06-05 23:19:13 +0100

    Removed gender column from table to stop clash with type

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604123643) do
+ActiveRecord::Schema.define(version: 20140605135034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ethnicities", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+  end
 
   create_table "messages", force: true do |t|
     t.string   "subject"
@@ -25,6 +31,13 @@ ActiveRecord::Schema.define(version: 20140604123643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "read",            default: false
+  end
+
+  create_table "user_ethnicities", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "ethnicity_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
@@ -45,7 +58,6 @@ ActiveRecord::Schema.define(version: 20140604123643) do
     t.string   "username"
     t.string   "type"
     t.integer  "age"
-    t.string   "gender"
     t.text     "about"
     t.string   "image_url"
     t.string   "preference"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,31 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-joe = User.find_or_create_by!(:id => "1") do |user|
+joe = Male.find_or_create_by!(:id => "1") do |user|
   user.email = "joe@email.com" 
   user.password = "password"
   user.username = "joey88"
   user.birthday = "16 Jun 1979"
-  user.gender = "male" 
-  user.preference = "female"
+  user.preference = "Female"
+  user.height = 180
+  user.image_url = "http://a403.idata.over-blog.com/232x300/5/01/45/64/Rouge.jpg"
+  user.about = "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
 end
 
-User.find_or_create_by!(:id => "2") do |user|
+Female.find_or_create_by!(:id => "2") do |user|
   user.email = "jenny@email.com" 
   user.password = "drowssap"
   user.username = "jenny88"
   user.birthday = "20 Mar 1978"
-  user.gender = "female" 
-  user.preference = "male"
+  user.preference = "Male"
+  user.height = 169
+  user.image_url = "http://thefuntimesguide.com/images/blogs/Ms_Green_MMs_on_Couch.jpg"
+  user.about = "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 end
 
 joe = User.first
-100.times do |i|
+10.times do |i|
 	@mess = Message.create(subject: "test#{i}", sender_username: "admin")
 	joe.messages << @mess
 end

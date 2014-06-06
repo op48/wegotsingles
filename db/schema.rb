a9612df2bf263c494c422d5aa6c2db46bdf1a894
Serafeim Maroulis <seramak@gmail.com>
2014-06-06 09:47:56 +0100

    Smoking attribute added to edit profile page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605152607) do
+ActiveRecord::Schema.define(version: 20140605170129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20140605152607) do
     t.string   "preference"
     t.date     "birthday"
     t.decimal  "height",                 precision: 6, scale: 2
+    t.boolean  "smoking"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

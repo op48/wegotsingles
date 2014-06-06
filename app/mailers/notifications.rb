a16650ea108a27f7e88a452b8a496f50e20a02db
Damilola Odelola <damiiodelola@gmail.com>
2014-06-06 15:34:43 +0100

    enabled method and set up associations/validations so that a male user recieves a email & message when they sign up AND any user receives an email when the user receives a message

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,9 +1,13 @@
-class MaleUserMail < ActionMailer::Base
+class Notifications < ActionMailer::Base
   default from: "lola@wegotsingles.com"
 
-def welcome(male)
+	def welcome(male)
     @greeting = "Hi Joe, thanks for joining us. Can't wait to start talking xx"
     mail to: "joe@example.com", subject: "Hi Joe ;)"
   end
 
+	def new_message(message)
+		@message = message
+		mail to: message.sender.email, subject: "New Message"
+	end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,16 +3,19 @@ class Message < ActiveRecord::Base
   belongs_to :original_message, :class_name => "Message", :foreign_key => :message_id
   belongs_to :sender, :class_name => "User", :foreign_key => :sender_id
   belongs_to :receiver, :class_name => "User", :foreign_key => :recipient_id
+  validates :sender_id, presence: true
+  validates :recipient_id, presence: true
 
   after_create :send_email
 	
 	def self.send_welcome_message(male)
-		@message = Message.new(:sender_username => "admin" ,:recipient_username => male.username, :subject => "Hi #{male.username} ;)", :body => "Hi #{male.username}, thanks for joining us. Can't wait to start talking xx" )
+		lola = Female.find_by(:username => "lola3")
+		@message = Message.new(:sender => lola ,:receiver => male, :subject => "Hi #{male.username} ;)", :body => "Hi #{male.username}, thanks for joining us. Can't wait to start talking xx" )
 	  @message.save!
-	  #MaleUserMail.welcome(male).deliver
+	  #Notifications.welcome(male).deliver
 	end
 
 	def send_email
-		
+		Notifications.new_message(self)
 	end
 end

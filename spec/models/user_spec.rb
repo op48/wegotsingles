112a9c92f07b389489bcf3f0774e9a8faa6efdd1
Zoltan Biber <zoltan.biber@gmail.com>
2014-06-06 10:37:12 +0100

    improved unit test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  it { should have_many(:messages) }
+  it { should have_many(:incoming_messages) }
+  it { should have_many(:outgoing_messages) }
   it { should validate_uniqueness_of(:username) }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:preference) }
-
 end

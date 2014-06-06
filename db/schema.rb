74b5504352c2422c34f9f0653f142ddcec6535b3
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-06 10:45:54 +0100

    MIGRATION: add name to horoscope

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606083219) do
+ActiveRecord::Schema.define(version: 20140606085541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140606083219) do
   create_table "horoscopes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "languages", force: true do |t|
